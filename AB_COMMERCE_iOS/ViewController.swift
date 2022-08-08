b7456f81713262b06c56aac679bc582835d39a53
Seoyeong <syheo@uno-solution.com>
2022-08-08 10:00:34 +0900

    Initial Commit

---
+++ AB_COMMERCE_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AB_COMMERCE_iOS
+//
+//  Created by rio on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ AB_COMMERCE_iOSUITests/AB_COMMERCE_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AB_COMMERCE_iOSUITestsLaunchTests.swift
+//  AB_COMMERCE_iOSUITests
+//
+//  Created by rio on 2022/08/08.
+//
+
+import XCTest
+
+class AB_COMMERCE_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
